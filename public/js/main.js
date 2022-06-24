68739ca36f1c5fec56688866a94634e9d280c47c
linds0202 <lindsayarainey@gmail.com>
2022-06-24 09:24:42 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+async function deleteItem(){
+    const iName = this.parentNode.childNodes[1].innerText
+    const iPrice = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemNameI': iName,
+              'iPrice': iPrice
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const iName = this.parentNode.childNodes[1].innerText
+    const iPrice = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemNameI': iName,
+              'iPrice': iPrice,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
